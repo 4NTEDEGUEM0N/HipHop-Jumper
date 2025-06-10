357971072d8665772a3e560fee4c4cdde1955d8d
4NTEDEGUEM0N <solbrilhan@gmail.com>
2025-06-10 15:27:04 -0300

    Movimento da Camera não ultrapassa o tamanho do tileMap

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,5 +1,6 @@
 #include "../include/Camera.hpp"
 #include "../include/Game.hpp"
+#include "../include/TileMap.hpp"
 
 #include <iostream>
 
@@ -20,7 +21,25 @@ void Camera::Unfollow() {
 
 void Camera::Update(float dt) {
     if (focus != nullptr) {
-        pos = focus->box.center() - Vec2(Game::VirtualScreenWidth / 2, Game::VirtualScreenHeight / 2);
+        Vec2 new_pos = focus->box.center() - Vec2(Game::VirtualScreenWidth / 2, Game::VirtualScreenHeight / 2);
+        GameObject* go = Game::GetInstance().GetState().GetTileMapObject();
+        TileMap* tileMap = nullptr;
+        if (go != nullptr)
+            tileMap = dynamic_cast<TileMap*>(go->GetComponent("TileMap"));
+        if (tileMap != nullptr) {
+            int height = tileMap->GetHeight();
+            int width = tileMap->GetWidth();
+            if (new_pos.X < 0)
+                new_pos.X = 0;
+            else if (new_pos.X >= width*tileMap->GetTileSetWidth() - Game::VirtualScreenWidth)
+                new_pos.X = width*tileMap->GetTileSetWidth() - Game::VirtualScreenWidth;
+            if (new_pos.Y < 0)
+                new_pos.Y = 0;
+            else if (new_pos.Y >= height*tileMap->GetTileSetHeight() - Game::VirtualScreenHeight)
+                new_pos.Y = height*tileMap->GetTileSetHeight() - Game::VirtualScreenHeight;
+        }
+
+        pos = new_pos;
     } else {
         speed = Vec2(0, 0);
         if (InputManager::GetInstance().IsKeyDown(UP_ARROW_KEY) || InputManager::GetInstance().IsKeyDown(SDLK_w)) {
